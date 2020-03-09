35078d2759c4453a50bcc6214ed03a1b6e624cb1
Coaco <naseeihility@163.com>
2020-03-09 23:18:46 +0800

    add tensorboard and double dqn

--- common/args.py
+++ common/args.py
@@ -4,7 +4,8 @@ import argparse
 def get_arg(params):
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cuda", default=True, action="store_false", help="Enable cuda")
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    parser.add_argument("-d", "--double", default=False, action="store_true", help="enable double dqn")
     parser.add_argument("-n", "--name", default=params.name, help="training name")
     parser.add_argument("--lr", default=params.learning_rate, help="learning_rate")
     parser.add_argument("--batch_size", default=params.batch_size, help="training batch_size")
---
+++ model/dqn_model.py
@@ -0,0 +1,106 @@
+import collections
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+
+
+class DQN(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(DQN, self).__init__()
+
+        self.convs = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+
+        self.full_connect = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.convs(Variable(torch.zeros(1, *shape)))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        x = x.float() / 256
+        output_conv = self.convs(x).view(x.size()[0], -1)
+        output = self.full_connect(output_conv)
+        return output
+
+
+Transition = collections.namedtuple('Transition', field_names=['state', 'action', 'reward', 'done', 'new_state'])
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.buffer = collections.deque(maxlen=capacity)
+        self.experience_source_iter = None
+        self.buffer = []
+        self.capacity = capacity
+        self.pos = 0
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def __iter__(self):
+        return iter(self.buffer)
+
+    def append(self, transition):
+        self.buffer.append(transition)
+
+    def sample(self, batch_size):
+        indices = np.random.choice(len(self.buffer), batch_size, replace=False)
+
+        # in order to acclerate calculate
+        states, actions, rewards, dones, next_states = zip(*[self.buffer[idx] for idx in indices])
+
+        return np.array(states), np.array(actions), np.array(rewards, dtype=np.float32), \
+               np.array(dones, dtype=np.uint8), np.array(next_states)
+
+
+class Agent:
+    def __init__(self, env, replay_buffer):
+        self.env = env
+        self.replay_buffer = replay_buffer
+        self._reset()
+
+    def _reset(self):
+        self.state = self.env.reset()
+        self.total_reward = 0.0
+
+    @torch.no_grad()
+    def play(self, net, epsilon=0.0, device="cpu"):
+        done_reward = None
+        if np.random.random() < epsilon:
+            # take a random action
+            action = self.env.action_space.sample()
+        else:
+            # get a max value aciton from the q-table
+            state = np.array([self.state], copy=False)
+            state_vector = torch.tensor(state).to(device)
+            qvals_vector = net(state_vector)
+            _, act_v = torch.max(qvals_vector, dim=1)
+            action = int(act_v.item())
+
+        # get transition from the environment
+        new_state, reward, is_done, _ = self.env.step(action)
+        self.total_reward += reward
+        self.state = new_state
+
+        # add transitions into replay buffer for later sample
+        trans = Transition(self.state, action, reward, is_done, new_state)
+        self.replay_buffer.append(trans)
+
+        if is_done:
+            done_reward = self.total_reward
+            self._reset()
+        return done_reward
